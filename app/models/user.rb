072c038853a751a533f386611ed455c0e8fc45cf
Pablo Rocha Moreira <pablo@fountain.com>
2019-11-16 15:26:29 -0800

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :email, :token, presence: true, uniqueness: true
-  has_many: emergency_contacts
-  
+  has_many: :emergency_contacts
+
   def initialize(params)
     super(params)
     set_token
