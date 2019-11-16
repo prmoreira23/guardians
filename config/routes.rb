abd4bb7efeb2876b4982abee3c951eeeb8285434
Pablo Rocha Moreira <pablo@fountain.com>
2019-11-16 15:34:36 -0800

    Add endpoint for message_received

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'webhook', to: 'application#verify_webhook'
+  post 'webhook', to: 'notification#message_received'
   post 'users', to: 'user#create'
 end
